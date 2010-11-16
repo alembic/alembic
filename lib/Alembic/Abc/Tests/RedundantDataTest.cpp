20dfb47414b13c92266b08e6f4023c9d57a1043a
Joe Ardent <alembic@ardent.nebcorp.com>
2010-11-15 16:24:58 -0800

    added a test to ensure that the fullname of objects is not lost with Lucas' changes to the HDF5 layer

---
+++ lib/Alembic/Abc/Tests/RedundantDataTest.cpp
@@ -0,0 +1,151 @@
+//-*****************************************************************************
+//
+// Copyright (c) 2009-2010,
+//  Sony Pictures Imageworks, Inc. and
+//  Industrial Light & Magic, a division of Lucasfilm Entertainment Company Ltd.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// *       Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// *       Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following disclaimer
+// in the documentation and/or other materials provided with the
+// distribution.
+// *       Neither the name of Sony Pictures Imageworks, nor
+// Industrial Light & Magic nor the names of their contributors may be used
+// to endorse or promote products derived from this software without specific
+// prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//-*****************************************************************************
+
+#include <Alembic/Abc/All.h>
+#include <Alembic/AbcCoreHDF5/All.h>
+
+#include <boost/lexical_cast.hpp>
+
+#include <set>
+
+#include "Assert.h"
+
+namespace Abc = Alembic::Abc;
+using namespace Abc;
+
+//-*****************************************************************************
+static std::vector<int32_t> intArraySamp;
+static std::set<std::string> PATHS;
+
+static const int32_t NUM_TOP_CHILDREN = 100;
+static const int32_t DEPTH = 499;
+
+typedef std::pair< std::set<std::string>::iterator, bool > PATH_PAIR;
+
+//-*****************************************************************************
+void makeIntArraySamp()
+{
+    for ( int32_t i = 0 ; i < 200 ; i++ )
+    {
+        intArraySamp.push_back( i );
+    }
+}
+
+//-*****************************************************************************
+void makeDeepHierarchy( OObject parent, const int level )
+{
+    if ( level > DEPTH )
+    {
+        OCompoundProperty p = parent.getProperties();
+        OInt32ArrayProperty iap( p, "intArrayProp", TimeSamplingType() );
+        iap.set( intArraySamp );
+        return;
+    }
+
+    makeDeepHierarchy( OObject( parent,
+                                boost::lexical_cast<std::string>( level ) ),
+                       level + 1 );
+}
+
+//-*****************************************************************************
+void readDeepHierarchy( IObject parent, const int level )
+{
+    if ( level > DEPTH )
+    {
+        ICompoundProperty p = parent.getProperties();
+        IInt32ArrayProperty iap( p, "intArrayProp" );
+        std::string fullName = const_cast<std::string&>(
+            iap.getObject().getFullName() );
+
+        PATH_PAIR ret = PATHS.insert( fullName );
+
+        TESTING_ASSERT( ret.second );
+
+        std::cout << fullName << std::endl << std::endl;
+
+        return;
+    }
+
+    readDeepHierarchy( IObject( parent,
+                                boost::lexical_cast<std::string>( level ) ),
+                       level + 1 );
+}
+
+//-*****************************************************************************
+void simpleTestOut( const std::string &iArchiveName )
+{
+    OArchive archive( Alembic::AbcCoreHDF5::WriteArchive(),
+                      iArchiveName );
+    OObject archiveTop( archive, kTop );
+
+    // create 100 top-level children
+    for ( int i = 0 ; i < NUM_TOP_CHILDREN ; i++ )
+    {
+        OObject obj( archiveTop, boost::lexical_cast<std::string>( i ) );
+        makeDeepHierarchy( obj, 0 );
+    }
+}
+
+//-*****************************************************************************
+void simpleTestIn( const std::string &iArchiveName )
+{
+    IArchive archive( Alembic::AbcCoreHDF5::ReadArchive(),
+                      iArchiveName, ErrorHandler::kThrowPolicy );
+
+    IObject archiveTop = archive.getTop();
+
+    TESTING_ASSERT( archiveTop.getNumChildren() == NUM_TOP_CHILDREN );
+
+    for ( int i = 0 ; i < NUM_TOP_CHILDREN ; i++ )
+    {
+        IObject obj( archiveTop, boost::lexical_cast<std::string>( i ) );
+        readDeepHierarchy( obj, 0 );
+    }
+}
+
+//-*****************************************************************************
+int main( int argc, char *argv[] )
+{
+    const std::string arkive( "redundantDataTest.abc" );
+
+    // make the int array sample values
+    makeIntArraySamp();
+
+    simpleTestOut( arkive );
+    simpleTestIn( arkive );
+
+    return 0;
+}
