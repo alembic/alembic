ce4fcc1d4a7ec4b3f78d93164fc5c3590d4c5cec
Joe Ardent <alembic@ardent.nebcorp.com>
2010-12-22 14:22:16 -0800

    fixed degrees vs. radians problem for Xform rotations in rman procedural

--- prman/Procedural/WriteGeo.cpp
+++ prman/Procedural/WriteGeo.cpp
@@ -132,8 +132,9 @@ void ProcessXform( IXform &xform, ProcArgs &args )
             {
                 RotateData rotateSample( sample );
                 V3d axis = rotateSample.getAxis();
-                RiRotate( rotateSample.getAngle(),
-                          axis.x, axis.y, axis.z );
+                // Xform stores rotation in radians, rman wants it in degrees
+                float degrees = 180.0 * rotateSample.getAngle() / M_PI;
+                RiRotate( degrees, axis.x, axis.y, axis.z );
                 break;
             }
             case kMatrixOperation:
