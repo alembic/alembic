e227bfb5668c616266c401beacc6ecbe8478f85b
Lucas Miller <miller.lucas@gmail.com>
2012-02-07 16:21:38 -0800

    Updating versions for 1.0.5.

--- lib/Alembic/AbcCoreAbstract/Foundation.h
+++ lib/Alembic/AbcCoreAbstract/Foundation.h
@@ -89,7 +89,7 @@ typedef float64_t chrono_t;
 //     to express data types and samples in Alembic
 
 //! Alembic version number Major/Minor/Patch XX.YY.ZZ
-#define ALEMBIC_LIBRARY_VERSION 10004
+#define ALEMBIC_LIBRARY_VERSION 10005
 
 //! Helper function which returns the version and date built in a string
 //! e.g. "Alembic 1.0.0 (built Jul  6 2011)"
