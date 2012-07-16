7c6988fd8b398a090b0f091bd4aeecf9c37f6bc0
Lucas Miller <miller.lucas@gmail.com>
2012-07-16 15:47:56 -0700

    Grabbing change from Alex, start at min time.

--- examples/bin/SimpleAbcViewer/Transport.cpp
+++ examples/bin/SimpleAbcViewer/Transport.cpp
@@ -44,7 +44,7 @@ Transport::Transport( const std::string &iAbcFileName,
   : m_scene( iAbcFileName )
   , m_framesPerSecond( iFramesPerSecond )
   , m_secondsPerFrame( 1.0f / iFramesPerSecond )
-  , m_currentSeconds( 0.0 )
+  , m_currentSeconds( m_scene.getMinTime() )
 {
     // Nothing
 }
