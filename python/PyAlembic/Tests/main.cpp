ede1912804a68086e1b4b897ab29e05a2523c94a
Lucas Miller <miller.lucas@gmail.com>
2012-08-03 11:43:50 -0700

    From jihunyu-dev
    Added testIterators.py
    Run testWrap.py, and testIterators.py

--- python/PyAlembic/Tests/main.cpp
+++ python/PyAlembic/Tests/main.cpp
@@ -80,6 +80,8 @@ runTests (int argc, char *argv[])
     // Test 5: Geom, Material and Collection bindings, iterators 
     {
         std::string code = 
+            "import testIterators\n"
+            "import testWrap\n"
             "import testAbcGeomBinding\n"
             "import testMaterial\n"
             "import testCollections\n";
