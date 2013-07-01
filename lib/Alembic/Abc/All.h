dbf77eb45b4fe244d4364d4af014eb85e025a091
Lucas Miller <miller.lucas@gmail.com>
2013-07-01 14:51:12 -0700

    Updating copyright year for this changed file.

--- lib/Alembic/Abc/All.h
+++ lib/Alembic/Abc/All.h
@@ -1,6 +1,6 @@
 //-*****************************************************************************
 //
-// Copyright (c) 2009-2011,
+// Copyright (c) 2009-2013,
 //  Sony Pictures Imageworks, Inc. and
 //  Industrial Light & Magic, a division of Lucasfilm Entertainment Company Ltd.
 //
