d6928eabab950d9927680e0a6e0befffe03a70a2
Lucas Miller <miller.lucas@gmail.com>
2013-07-16 14:40:45 -0700

    Adding a unit test to check the various combinations of constant and animated values and indices on indexed geom params.

--- lib/Alembic/AbcGeom/Tests/GeomBaseTest.cpp
+++ lib/Alembic/AbcGeom/Tests/GeomBaseTest.cpp
@@ -138,7 +138,219 @@ void Example1_GeomBaseIn()
 
 }
 
+void IndexexedGeomParamTest()
+{
+    {
+        std::vector< std::string > vals( 4 );
+        vals[0] = "a";
+        vals[1] = "b";
+        vals[2] = "c";
+        vals[3] = "d";
 
+        std::vector< Alembic::Util::uint32_t > indices( 4 );
+        indices[0] = 0;
+        indices[1] = 1;
+        indices[2] = 2;
+        indices[3] = 3;
+
+        StringArraySample valsSamp( vals );
+        UInt32ArraySample indicesSamp( indices );
+
+        OStringGeomParam::Sample samp( valsSamp, indicesSamp,
+                                       Alembic::AbcGeom::kConstantScope );
+
+        OArchive archive( Alembic::AbcCoreHDF5::WriteArchive(),
+                          "indexedGeomParam.abc" );
+        OCompoundProperty prop = archive.getTop().getProperties();
+        TimeSamplingPtr ts( new TimeSampling( 1.0 / 24.0, 0.0 ) );
+
+        // constant values, constant indices
+        OStringGeomParam cvci( prop, "cvci", true,
+                               Alembic::AbcGeom::kConstantScope, 1, ts );
+        cvci.set( samp );
+        cvci.set( samp );
+
+        // animated values, constant indices
+        OStringGeomParam avci( prop, "avci", true,
+                               Alembic::AbcGeom::kConstantScope, 1, ts );
+        avci.set( samp );
+        vals[3] = "e";
+        avci.set( samp );
+        vals[3] = "d";
+
+        // constant values, animated indices
+        OStringGeomParam cvai( prop, "cvai", true,
+                               Alembic::AbcGeom::kConstantScope, 1, ts );
+        cvai.set( samp );
+        indices[3] = 0;
+        cvai.set( samp );
+        indices[3] = 3;
+
+        // animated values, animated indices
+        OStringGeomParam avai( prop, "avai", true,
+                               Alembic::AbcGeom::kConstantScope, 1, ts );
+        avai.set( samp );
+        vals[0] = "aa";
+        indices[3] = 0;
+        avai.set( samp );
+
+    }
+
+    {
+        IArchive archive( Alembic::AbcCoreHDF5::ReadArchive(),
+                          "indexedGeomParam.abc" );
+        ICompoundProperty prop = archive.getTop().getProperties();
+        IStringGeomParam::Sample samp;
+
+        IStringGeomParam cvci( prop, "cvci" );
+        cvci.getExpanded(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        cvci.getExpanded(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+
+        cvci.getIndexed(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 3 );
+        cvci.getIndexed(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 3 );
+
+        IStringGeomParam avci( prop, "avci" );
+        avci.getExpanded(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        avci.getExpanded(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "e" );
+
+        avci.getIndexed(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 3 );
+        avci.getIndexed(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "e" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 3 );
+
+        IStringGeomParam cvai( prop, "cvai" );
+        cvai.getExpanded(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        cvai.getExpanded(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "a" );
+
+        cvai.getIndexed(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 3 );
+        cvai.getIndexed(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 0 );
+
+        IStringGeomParam avai( prop, "avai" );
+        avai.getExpanded(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        avai.getExpanded(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "aa" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "aa" );
+
+        avai.getIndexed(samp, ISampleSelector( 0.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "a" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 3 );
+        avai.getIndexed(samp, ISampleSelector( 1.0/24.0 ) );
+        TESTING_ASSERT(
+            samp.getVals()->get()[0] == "aa" &&
+            samp.getVals()->get()[1] == "b" &&
+            samp.getVals()->get()[2] == "c" &&
+            samp.getVals()->get()[3] == "d" );
+        TESTING_ASSERT(
+            samp.getIndices()->get()[0] == 0 &&
+            samp.getIndices()->get()[1] == 1 &&
+            samp.getIndices()->get()[2] == 2 &&
+            samp.getIndices()->get()[3] == 0 );
+    }
+}
 
 //-*****************************************************************************
 //-*****************************************************************************
@@ -162,5 +374,6 @@ int main( int argc, char *argv[] )
 
     Example1_GeomBaseIn();
 
+    IndexexedGeomParamTest();
     return 0;
 }
