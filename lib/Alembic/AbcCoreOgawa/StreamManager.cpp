18ae1f5132b9c1ddb63e33a303b11c1a7cfcc2bf
Lucas Miller <miller.lucas@gmail.com>
2013-11-05 10:12:55 -0800

    __sync_bool_compare_and_swap might not work correctly gcc versions prior to 4.4
    http://gcc.gnu.org/bugzilla/show_bug.cgi?id=37651

--- lib/Alembic/AbcCoreOgawa/StreamManager.cpp
+++ lib/Alembic/AbcCoreOgawa/StreamManager.cpp
@@ -69,7 +69,7 @@ StreamManager::~StreamManager()
 {
 }
 
-#if !defined(__APPLE__) && defined(__GNUC__) && __GNUC__ > 3
+#if !defined(__APPLE__) && defined(__GNUC__) && __GNUC__ >= 4 && __GNUC_MINOR__ >= 4
 
 StreamIDPtr StreamManager::get()
 {
